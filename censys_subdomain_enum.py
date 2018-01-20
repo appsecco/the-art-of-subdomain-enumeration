b70e6b08d3cc2872d67dc16ccc0cabfb1a978ec3
synster <you@example.com>
2018-01-20 08:00:46 +0530

    Updated censys script

--- censys_subdomain_enum.py
+++ censys_subdomain_enum.py
@@ -24,38 +24,47 @@ import sys
 
 try:
     import censys.certificates
+    import censys.ipv4
 except ImportError:
     logging.info("\033[1;31m[!] Failed to import censys module. Run 'pip install censys'\033[1;m")
     sys.exit()
 
-def get_certificates(domain):
-    if not CENSYS_API_ID or not CENSYS_API_SECRET:
-        logging.info("\033[1;31m[!] API KEY or Secret for Censys not provided.\033[1;m" \
-                     "\nYou'll have to provide them in the script") 
-        sys.exit()
-    logging.info("[+] Extracting certificates for {} using Censys".format(domain))
-    c = censys.certificates.CensysCertificates(CENSYS_API_ID, CENSYS_API_SECRET)
-    search_results = c.paged_search(domain)
-    certificates = search_results['results']
-    if len(certificates) == 0:
-        print("\033[1;31m[!] No matching certificates found!\033[1;m")
-        sys.exit()
-    return certificates
+def get_certificates():
+    try:
+        if not CENSYS_API_ID or not CENSYS_API_SECRET:
+            logging.info("\033[1;31m[!] API KEY or Secret for Censys not provided.\033[1;m" \
+                        "\nYou'll have to provide them in the script") 
+            sys.exit()
+        logging.info("[+] Extracting certificates for {} using Censys".format(domain))
+        censys_certificates = censys.certificates.CensysCertificates(CENSYS_API_ID, CENSYS_API_SECRET)
+        return censys_certificates
+    except censys.base.CensysUnauthorizedException:
+        logging.info('[!] Your Censys credentials look invalid.\n')
+        exit(1)
+    except censys.base.CensysRateLimitExceededException:
+        logging.info('[!] Looks like you exceeded your Censys account limits rate. Exiting\n')
+        exit(1)
 
 def get_subdomains(domain, certificates):
     logging.info("[+] Extracting sub-domains for {} from certificates".format(domain))
     subdomains = []
-    for certificate in certificates:
-        parsed_result = re.findall(r'(?<=CN=).*', certificate[u'parsed.subject_dn'])
-        if len(parsed_result) > 0 and domain in parsed_result[0]: subdomains.append(parsed_result[0])
-    return subdomains
+    certificate_query = 'parsed.names: {}'.format(domain)
+    certificates_search_results = certificates.search(certificate_query, fields=['parsed.names'])
+    for search_result in certificates_search_results:
+        subdomains.extend(search_result['parsed.names'])
+    return set(subdomains)
 
-def print_subdomains(subdomains):
-    unique_subdomains = list(set(subdomains))
-    print("\033[1;32m[+] Total unique subdomains found: {}\033[1;m".format(len(unique_subdomains)))
-    print("[+] List of subdomains extracted:\n")
-    for sub_domain in unique_subdomains:
-        print(sub_domain)
+def print_subdomains(subdomains, domain):
+    unique_subdomains = []
+    if len(subdomains) is 0:
+        logging.info('[!] Did not find any subdomains')
+        return
+    for subdomain in subdomains:
+        if '*' not in subdomain and subdomain.endswith(domain): 
+            unique_subdomains.append(subdomain)
+    logging.info("\033[1;32m[+] Total unique subdomains found: {}\033[1;m".format(len(unique_subdomains)))
+    for subdomain in sorted(unique_subdomains):
+        print(subdomain)
 
 def get_domain():
     if len(sys.argv) < 2:
@@ -67,6 +76,6 @@ def get_domain():
 
 if __name__ == '__main__':
     domain = get_domain()
-    certificates = get_certificates(domain)
+    certificates = get_certificates()
     subdomains = get_subdomains(domain, certificates)
-    print_subdomains(subdomains)
+    print_subdomains(subdomains, domain)
