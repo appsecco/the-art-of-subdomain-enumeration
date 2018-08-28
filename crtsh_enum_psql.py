8f31e57080981fe10613c196c50fae54b6be1128
Bharath <bharath.isecure@protonmail.com>
2018-08-28 19:30:42 +0530

    Updated DB connection with pooling, Updated CNAME resolution, Removed stdout output

--- crtsh_enum_psql.py
+++ crtsh_enum_psql.py
@@ -8,27 +8,36 @@ try:
 except ImportError:
     raise ImportError('\n\033[33mpsycopg2 library missing. pip install psycopg2\033[1;m\n')
     sys.exit(1)
+
 try:
     import click
 except ImportError:
-    raise ImportError('\n\033[33mpsycopg2 library missing. pip install click\033[1;m\n')
+    raise ImportError('\n\033[33mclick library missing. pip install click\033[1;m\n')
     sys.exit(1)
 import re
 import sys
 import json
-from socket import gethostbyname, gaierror
+import logging
+from os.path import abspath
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s"
+    )
 
 DB_HOST = 'crt.sh'
 DB_NAME = 'certwatch'
 DB_USER = 'guest'
+DB_PASSWORD = ''
 
 def connect_to_db(domain_name):
     try:
         conn = psycopg2.connect("dbname={0} user={1} host={2}".format(DB_NAME, DB_USER, DB_HOST))
+        conn.autocommit = True
         cursor = conn.cursor()
         cursor.execute("SELECT ci.NAME_VALUE NAME_VALUE FROM certificate_identity ci WHERE ci.NAME_TYPE = 'dNSName' AND reverse(lower(ci.NAME_VALUE)) LIKE reverse(lower('%{}'));".format(domain_name))
     except:
-        print("\n\033[1;31m[!] Unable to connect to the database\n\033[1;m")
+        logging.info("\n\033[1;31m[!] Unable to connect to the database\n\033[1;m")
     return cursor
 
 def get_unique_domains(cursor, domain_name):
@@ -41,36 +50,56 @@ def get_unique_domains(cursor, domain_name):
                     unique_domains.append(subdomain)
     return unique_domains
 
-def print_unique_domains(unique_domains):
-    for unique_domain in sorted(unique_domains):
-        print(unique_domain)
-
-def get_domain_name():
-    if len(sys.argv) <= 1:
-        print("\n\033[33mUsage: python crtsh_enum_psql.py <target_domain>\033[1;m\n")
-        sys.exit(1)
-    else:
-        return sys.argv[1]
-
 def do_dns_resolution(unique_domains):
+    try:
+        import dns.resolver
+    except ImportError:
+        raise ImportError('\n\033[33mdnspython library missing. pip install dnspython\033[1;m\n')
+        sys.exit(1)
     dns_resolution_results = {}
     for domain in set(unique_domains):
         domain = domain.replace('*.','')
+        dns_resolution_results[domain] = {}
         try:
-            ip_address = gethostbyname(domain)
-            dns_resolution_results[domain] = ip_address
-            #print("\033[92m{0:<50} - {1:20}\033[1;m".format(domain,ip_address.rstrip("\n\r")))
-        except gaierror as e:
-            #print("\033[93m{0:<50} - {1:20}\033[1;m".format(domain,"No A record exists",end=''))
-            dns_resolution_results[domain] = "No A record exists"
-            #print(e.message)
+            for qtype in ['A','CNAME']:
+                dns_answer = dns.resolver.query(domain,qtype, raise_on_no_answer=False)
+                if dns_answer.rrset is None:
+                    pass
+                elif dns_answer.rdtype == 1:
+                    a_records = [str(i) for i in dns_answer.rrset]
+                    dns_resolution_results[domain]["A"] = a_records
+                elif dns_answer.rdtype == 5:
+                    cname_records = [str(i) for i in dns_answer.rrset]
+                    dns_resolution_results[domain]["CNAME"] = cname_records
+                else:
+                    dns_resolution_results[domain]["A"] = "bla"
+                    dns_resolution_results[domain]["CNAME"] = "bla bla"
+        except dns.resolver.NXDOMAIN:
+            dns_resolution_results[domain]["A"] = "No such domain"
+            pass
+        except dns.resolver.Timeout:
+            dns_resolution_results[domain]["A"] = "Timed out while resolving"
+            dns_resolution_results[domain]["CNAME"] = "Timed out error while resolving"
+            pass
+        except dns.exception.DNSException:
+            dns_resolution_results[domain]["A"] = "Unknown error while resolving"
+            dns_resolution_results[domain]["CNAME"] = "Unknown error while resolving"
+            pass
     return dns_resolution_results
+
+def print_json_results(domain,dns_resolution_results):
+    print(json.dumps(dns_resolution_results))
+    results_file = "{}_results.json".format(domain)
+    with open(results_file, 'w+') as results_file:
+        json.dump(dns_resolution_results, results_file, default=str)
+        file_path = abspath(results_file.name)
+    logging.info("\033[1;32m[+] Results written to JSON file : {}\033[1;m".format(file_path))
 
 @click.command()
 @click.argument('domain')
 @click.option('--resolve/--no-resolve','-r', default=False,
                 help='Enable/Disable DNS resolution')
-@click.option('--output','-o',
+@click.option('--output','-o', default='json',
                 help='Output in JSON format')
 def main(domain, resolve, output):
     domain_name = domain
@@ -82,15 +111,7 @@ def main(domain, resolve, output):
         sys.exit()
     else:
         dns_resolution_results = do_dns_resolution(unique_domains)
-
-    if output=='json':
-        print(json.dumps(dns_resolution_results))
-    else:
-        for domain,ip_address in dns_resolution_results.iteritems():
-            if ip_address == "No A record exists":
-                print("\033[93m{0:<50} - {1:20}\033[1;m".format(domain,ip_address.rstrip("\n\r")))
-            else:
-                print("\033[92m{0:<50} - {1:20}\033[1;m".format(domain,ip_address.rstrip("\n\r")))
+        print_json_results(domain,dns_resolution_results)
 
 if __name__ == '__main__':
     main()
